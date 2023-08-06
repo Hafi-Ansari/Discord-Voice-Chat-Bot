831c90f4a2aeef08ecdc885d102d4d662a3bf6dc
hafi-ansari <hafi.m.ansari@gmail.com>
2023-08-05 22:11:13 -0500

    Chat-Bot can successfully interfaces with Discord

--- Chat-Bot/server.py
+++ Chat-Bot/server.py
@@ -1,18 +1,36 @@
-from flask import Flask, request, jsonify 
-from flask import request
+from flask import Flask, request, jsonify
+import os
+import openai
+from dotenv import load_dotenv
 
+load_dotenv()
+openai.api_key = os.getenv("GPT_TOKEN")
 
 app = Flask(__name__)
 
+def generate_gpt_response(user_message):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an AI model roleplaying as Ronnie Coleman, the professional bodybuilder. You use Ronnie Coleman's slang in two sentence answers."},
+            {"role": "user", "content": user_message},
+        ],
+        max_tokens=60,
+        temperature=1
+    )
+    return response['choices'][0]['message']['content']
+
 @app.route('/message', methods=['POST'])
-def log_message():
+def log_and_respond_message():
     content = request.json
     user_message = content['message']
     print(f"Received message: {user_message}")  # Logging the user's message
 
+    gpt_response = generate_gpt_response(user_message)
+
     response = {
         "status": "success",
-        "message": f"Received and logged \"{user_message}\""
+        "message": gpt_response
     }
     return jsonify(response)
 
